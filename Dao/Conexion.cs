e15b5c7d709fe3b0b0c7646593f108d5fce79798
willynar <33671414+willynar@users.noreply.github.com>
2019-08-29 21:59:58 -0500

    Lectura inicial

--- Dao/Conexion.cs
+++ Dao/Conexion.cs
@@ -14,6 +14,7 @@ namespace practica_crud.Dao
             string mensaje;
             try
             {
+                //SectionInformation crea la conexion
                 using (var conexion = new SqlConnection(ConfigurationManager.ConnectionStrings["conexion"].ToString()))
                 {
                     using (var comando = new SqlCommand(nombre, conexion))
@@ -21,10 +22,12 @@ namespace practica_crud.Dao
                         using (var adapter = new SqlDataAdapter(comando))
                         {
                             comando.CommandType = CommandType.StoredProcedure;
+                            //si se tienen parametros se envian al procedimiento almacenado
                             if (parametros != null)
                                 foreach (SqlParameter item in parametros)
                                     comando.Parameters.Add(dataset);
-                            conexion.Open();
+                            //SectionInformation abre la conexion
+                            conexion.Open();                            
                             adapter.Fill(dataset);
                             comando.Parameters.Clear();
                             conexion.Close();
@@ -34,11 +37,13 @@ namespace practica_crud.Dao
             }
             catch (Exception e)
             {
+                //SectionInformation debuelde un dataset con una tabla y el mensaje de error
                 mensaje = e.Message;
                 datatable.Columns.Add("error");
                 datatable.Rows.Add(mensaje);
                 dataset.Tables.Add(datatable);
             }
+            //SectionInformation retorna el dataset
             return dataset;
         }
 
--- Form1.cs
+++ Form1.cs
@@ -16,11 +16,14 @@ namespace practica_crud
         private void Form1_Load(object sender, EventArgs e)
         {
             string variable = "hola";
+            //sender invoca la clase data acces objet
             Conexion conex = new Conexion();
+            //sender crea un array sqlparametros y se llena con los datos a enviar al procedimiento almacenado
             SqlParameter[] parametros =
             {
                 new SqlParameter("@nombre",variable)
             };
+            //se envian los parametros y el nombre de el procedimeinto almacenado al dao
             DataSet result = conex.ejecutarprocedimiento("procedimiento", parametros);
         }
     }
